648474b7b9ef835d33a9c5f0d4b453b2e960d53f
kmeersman <katie4682@yahoo.com>
2020-07-10 16:11:50 -0600

    deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../App.css";
+import headerimg from "../img/headerimg.png"
 
 function Header() {
   return (
     <div>
-      <img className="header-img" alt="header" src={process.env.PUBLIC_URL + "/img/headerimg.PNG"} fluid />
+      <img className="header-img" alt="header" src={headerimg} fluid />
     </div>
   );
 }
