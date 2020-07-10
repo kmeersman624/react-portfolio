fedd762942de4fe2331a41a8f770b0971342cfdf
kmeersman <katie4682@yahoo.com>
2020-07-10 10:33:45 -0600

    Updates

---
+++ precache-manifest.524260783a97d97aa93c8c1e53dba5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e676a612fae96e66da26fa8a7d5caa",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "678e286d1853defd44e9",
+    "url": "/React-Portfolio/static/css/main.110547a8.chunk.css"
+  },
+  {
+    "revision": "4d38760e6c3a6906041a",
+    "url": "/React-Portfolio/static/js/2.e327514c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.e327514c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678e286d1853defd44e9",
+    "url": "/React-Portfolio/static/js/main.d4ada09d.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
