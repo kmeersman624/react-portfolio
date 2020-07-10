c59be4431d31bfb999d30271102ee8ee5365e68f
kmeersman <katie4682@yahoo.com>
2020-07-10 10:55:32 -0600

    trying to fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 function Header() {
-    return (
-        <div>
-            <img className="header-img" alt="header" src="/img/headerimg.PNG" fluid />
-        </div>
-    )
+  return (
+    <div>
+      <img className="header-img" alt="header" src={process.env.PUBLIC_URL + "/img/headerimg.PNG"} fluid />
+    </div>
+  );
 }
 
-export default Header
+export default Header;
