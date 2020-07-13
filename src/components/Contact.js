4d20b84a21ea21dbff6f94c218af773f73f43201
kmeersman <katie4682@yahoo.com>
2020-07-13 11:08:19 -0600

    changed text styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 
 function Contact() {
   return (
@@ -8,19 +9,19 @@ function Contact() {
           <form>
             <div class="contactinfo">
               <h2>Get in touch with Kate:</h2>
-              <a href="https://github.com/kmeersman624">
+              <a class="links" href="https://github.com/kmeersman624">
                 <i className="fab fa-github"></i> GitHub Profile
               </a>
               <br />
-              <a href="https://www.linkedin.com/in/kate-meersman-2a66a2155/">
+              <a class="links" href="https://www.linkedin.com/in/kate-meersman-2a66a2155/">
                 <i className="fab fa-linkedin"></i> LinkedIn Profile
               </a>
               <br />
-              <a href="/img/resume.pdf">
+              <a class="links" href="/img/resume.pdf">
                 <i class="fas fa-file-pdf"></i> Resume
               </a>
               <br />
-              <a href="mailto:katie4682@yahoo.com">
+              <a class="links" href="mailto:katie4682@yahoo.com">
                 <i class="fas fa-envelope"></i> Email Me!
               </a>
             </div>
