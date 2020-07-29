0536a74b9782f6c71b817f5559c2e138211a979c
kmeersman <katie4682@yahoo.com>
2020-07-28 20:54:04 -0600

    updated all project cards with react bootstrap

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,129 +43,116 @@ function Projects() {
           <Card.Body>
             <Card.Title>COVID Date Night</Card.Title>
             <Card.Text>
-              Application uses JavaScript and jQuery to make AJAX calls to 3rd party APIs utilizing Foudation CSS framework.
+              Application uses JavaScript and jQuery to make AJAX calls to 3rd
+              party APIs utilizing Foudation CSS framework.
             </Card.Text>
-              <div className="card-links">
-                <div className="github-link">
-                  <a href="https://kmeersman624.github.io/Project-1/">
-                    {" "}
-                    GitHub{" "}
-                  </a>
-                </div>
-                <div className="online-link">
-                  <a href="https://kmeersman624.github.io/Project-1/"> Live </a>
-                </div>
+            <div className="card-links">
+              <div className="github-link">
+                <a href="https://kmeersman624.github.io/Project-1/"> GitHub </a>
               </div>
-              </Card.Body>
+              <div className="online-link">
+                <a href="https://kmeersman624.github.io/Project-1/"> Live </a>
+              </div>
+            </div>
+          </Card.Body>
         </Card>
-          <div className="card">
-            <div className="card-front">
-              <div className="card-title">
-                <h5>Open Sesame</h5>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={login} />
+          <Card.Body>
+            <Card.Title>Open Sesame</Card.Title>
+            <Card.Text>
+              User can utilize Open Sesame as a secure location to store all
+              usernames and passwords. Built with JavaScript, Handlbars, jQuery,
+              Sequelize, Bootstrap CSS.
+            </Card.Text>
+            <div className="card-links">
+              <div className="github-link">
+                <a href="https://github.com/MarcusTravis/Open-Sesame">
+                  {" "}
+                  GitHub{" "}
+                </a>
               </div>
-              <div className="project-image">
-                <img
-                  src={login}
-                  alt="login page for password database"
-                  className="project-image"
-                />
-              </div>
-              <div className="card-links">
-                <div className="github-link">
-                  <a href="https://github.com/MarcusTravis/Open-Sesame">
-                    {" "}
-                    GitHub{" "}
-                  </a>
-                </div>
-                <div className="online-link">
-                  <a href="https://safe-badlands-31285.herokuapp.com/">
-                    {" "}
-                    Live{" "}
-                  </a>
-                </div>
+              <div className="online-link">
+                <a href="https://safe-badlands-31285.herokuapp.com/"> Live </a>
               </div>
             </div>
-          </div>
-          <div className="card">
-            <div className="card-front">
-              <div className="card-title">
-                <h5>React Employee Directory</h5>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={directory} />
+          <Card.Body>
+            <Card.Title>React Employee Directory</Card.Title>
+            <Card.Text>
+              This is an employee directory with React. It breaks up your
+              application's UI into components, manages component state, and
+              responds to user events.
+            </Card.Text>
+            <div className="card-links">
+              <div className="github-link">
+                <a href="https://github.com/kmeersman624/React-Employee-Directory">
+                  {" "}
+                  GitHub{" "}
+                </a>
               </div>
-              <div className="project-image">
-                <img
-                  src={directory}
-                  alt="directory"
-                  className="project-image"
-                />
-              </div>
-              <div className="card-links">
-                <div className="github-link">
-                  <a href="https://github.com/kmeersman624/React-Employee-Directory">
-                    {" "}
-                    GitHub{" "}
-                  </a>
-                </div>
-                <div className="online-link">
-                  <a href="https://kmeersman624.github.io/React-Employee-Directory/">
-                    {" "}
-                    Live{" "}
-                  </a>
-                </div>
+              <div className="online-link">
+                <a href="https://kmeersman624.github.io/React-Employee-Directory/">
+                  {" "}
+                  Live{" "}
+                </a>
               </div>
             </div>
-          </div>
-          <div className="card">
-            <div className="card-front">
-              <div className="card-title">
-                <h5>Online/Offline Budget</h5>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={budget} />
+          <Card.Body>
+            <Card.Title>Online/Offline Budget</Card.Title>
+            <Card.Text>
+              PWA aplication built with NodeJS where the user can add expenses
+              and deposits to their budget with or without a connection. When
+              entering transactions offline, the total populates when brought
+              back online.
+            </Card.Text>
+            <div className="card-links">
+              <div className="github-link">
+                <a href="https://github.com/kmeersman624/Online-Offline-Budget-Trackers">
+                  {" "}
+                  GitHub{" "}
+                </a>
               </div>
-              <div className="project-image">
-                <img src={budget} alt="budget" className="project-image" />
-              </div>
-              <div className="card-links">
-                <div className="github-link">
-                  <a href="https://github.com/kmeersman624/Online-Offline-Budget-Trackers">
-                    {" "}
-                    GitHub{" "}
-                  </a>
-                </div>
-                <div className="online-link">
-                  <a href="https://secure-eyrie-61403.herokuapp.com/"> Live </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-front">
-              <div className="card-title">
-                <h5>Exercise Tracker</h5>
-              </div>
-              <div className="project-image">
-                <img
-                  src={workout}
-                  alt="workout dashboard"
-                  className="img-thumbnail thumbnail"
-                />
-              </div>
-              <div className="card-links">
-                <div className="github-link">
-                  <a href="https://github.com/kmeersman624/Workout-Tracker">
-                    {" "}
-                    GitHub{" "}
-                  </a>
-                </div>
-                <div className="online-link">
-                  <a href="https://murmuring-scrubland-28081.herokuapp.com/">
-                    {" "}
-                    Live{" "}
-                  </a>
-                </div>
+              <div className="online-link">
+                <a href="https://secure-eyrie-61403.herokuapp.com/"> Live </a>
               </div>
             </div>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={workout} />
+          <Card.Body>
+            <Card.Title>Exercise Tracker</Card.Title>
+            <Card.Text>
+              This is a fitness tracker application allows the user to view,
+              create, and track daily workouts. It uses a Mongo database with a
+              Mongoose schema and handles routes with Express.
+            </Card.Text>
+            <div className="card-links">
+              <div className="github-link">
+                <a href="https://github.com/kmeersman624/Workout-Tracker">
+                  {" "}
+                  GitHub{" "}
+                </a>
+              </div>
+              <div className="online-link">
+                <a href="https://murmuring-scrubland-28081.herokuapp.com/">
+                  {" "}
+                  Live{" "}
+                </a>
+              </div>
             </div>
-            </div>
-            </div>
-
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   );
 }
 
