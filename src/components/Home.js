e28846b379c5d52f65503ee5ee020996daa3d88b
kmeersman <katie4682@yahoo.com>
2020-08-04 12:51:57 -0600

    update about me to include container and rows/cols from react-bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "../App.css";
 import katebw from "../img/katebw.jpg";
-
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function Home() {
   return (
     <div className="center">
-      <main className="container-fluid m-5 bg-light">
+      <Container className="container-fluid m-5 bg-light">
         <header className="about">
           <h1>About Me</h1>
         </header>
-        <div className="row  mb-5">
-          <div className="profile col-md-3">
-            <img className="kateimg" alt="kate" src={katebw} />
-          </div>
-          <div className="about col-md-7 h5">
+        <Row className="row  mb-5">
+          <Col className="profile col-md-3">
+            <Image className="kateimg" alt="kate" src={katebw} fluid />
+          </Col>
+          <Col className="about col-md-5 h5">
             I was born and raised in Colorado, which is where I continue to
             reside, after a few stints in Maui, Calgary, and Vail. I originally
             went to college for hospitality, focusing on a career in Event Sales
@@ -24,35 +24,44 @@ function Home() {
             love the satisfaction of conquering a difficult task. Please check
             out my project portfolio for recent applications I have been working
             on. Reach out directly for more information!
+          </Col>
+        </Row>
+        <Row className="row">
+          <Col className="col-md-12">
+            <h5>
+              <b>Technical Skills:</b>
+            </h5>
+            Computer Science applied to JavaScript:{" "}
+            <i>Design Patterns • Algorithms</i>
             <br />
             <br />
-            <div className="row">
-              <div className="col-md-12">
-                <h5><b>Technical Skills:</b></h5>
-                Computer Science applied to JavaScript: <i>Design Patterns • Algorithms</i>
-                <br />
-                <br />
-                Browser Based Technologies: <i>HTML • CSS • JavaScript • jQuery • Responsive Design • Bootstrap • Handlebars </i>
-                <br />
-                Deployment: <i>Heroku • Git</i>
-                <br />
-                <br />
-                Databases: <i>MySQL • MongoDB</i>
-                <br />
-                <br />
-                Server-Side Development: <i>Express • Security and Session Storage • User Authentication • MERN Stack (MongoDB, Express.js, React.js, Node.js)</i>
-                <br />
-                <br />
-                Quality Assurance: <i>Writing Tests</i>
-                <br />
-                <br />
-               Internet Marketing: <i>Semantic HTML</i>
-                <br />
-              </div>
-            </div>
-          </div>
-          </div>
-      </main>
+            Browser Based Technologies:{" "}
+            <i>
+              HTML • CSS • JavaScript • jQuery • Responsive Design • Bootstrap •
+              Handlebars{" "}
+            </i>
+            <br />
+            Deployment: <i>Heroku • Git</i>
+            <br />
+            <br />
+            Databases: <i>MySQL • MongoDB</i>
+            <br />
+            <br />
+            Server-Side Development:{" "}
+            <i>
+              Express • Security and Session Storage • User Authentication •
+              MERN Stack (MongoDB, Express.js, React.js, Node.js)
+            </i>
+            <br />
+            <br />
+            Quality Assurance: <i>Writing Tests</i>
+            <br />
+            <br />
+            Internet Marketing: <i>Semantic HTML</i>
+            <br />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
